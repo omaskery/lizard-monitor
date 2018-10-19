c5f1f79385521b8b7e407d43b53abb9db1a6d408
Oliver Maskery <omaskery@googlemail.com>
2018-10-19 20:18:29 +0100

    fixes missing pull when changing HEAD
    when performing a checkout it would forget to perform a git pull,
    this fixes that :fingers_crossed:

--- lizard-mon.py
+++ lizard-mon.py
@@ -169,9 +169,11 @@ def get_repo(repos_dir: str, name: str, repo_info: lizard_mon.config.RepositoryI
             if repo.head.is_detached or repo.active_branch.name != repo_info.branch:
                 print(f"  checking out branch {repo_info.branch}")
                 repo.git.checkout(repo_info.branch)
-            else:
+            if not repo.head.is_detached:
                 print(f"  ensuring we're up to date on our current branch")
                 remote.pull()
+            else:
+                print(f"  not doing a pull because we're on detached HEAD")
         else:
             commit_hash_at_date = repo.git.rev_list("-1", f'--before="{at_date}"', repo_info.branch)
             if not commit_hash_at_date:
